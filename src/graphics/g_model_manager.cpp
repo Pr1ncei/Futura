85d1e37505c76f2218fb580e6c14432834d1304b
Pr1ncei <princei.cs50@gmail.com>
2025-12-20 23:11:10 +0800

    Initial Model Manager Implementation

--- src/graphics/g_model_manager.cpp
+++ src/graphics/g_model_manager.cpp
@@ -1,2 +1,30 @@
 #include "g_model_manager.h"
+#include "g_model.h"
+#include "../utils/u_log.h"
 
+std::unordered_map<std::string, std::shared_ptr<Model>> ModelManager::s_models;
+
+
+std::shared_ptr<Model> ModelManager::Load(const std::string &path)
+{
+    auto it = s_models.find(path);
+    if (it != s_models.end())
+        return it->second;
+
+
+    // havent added assimp yet
+    //std::shared_ptr<Model> model = std::make_shared<Model>(LoadAssimpFromFile(path));
+
+    // s_models[path] = model; 
+    // return model;
+}
+
+std::shared_ptr<Model> ModelManager::Get(const std::string &path)
+{
+    return s_models[path];
+}
+
+void ModelManager::Clear()
+{   
+    s_models.clear();
+}
--- src/graphics/g_model_manager.h
+++ src/graphics/g_model_manager.h
@@ -1,17 +1,22 @@
-// // Initial Model Manager that manages all the models
+// Initial Model Manager that manages all the models
 
-// #include <GLFW/glfw3.h>
-// #include <unordered_map>
-// #include <string>
-// #include "../graphics/g_model.h"
-// #include "../graphics/g_shader.h"
+#ifndef MODEL_MANAGER_h
+#define MODEL_MANAGER_H
 
-// class ModelManager
-// {
-// public:
-//     void Load(const std::string &name, const std::string &path); 
-//     Model& Get(const std::string &name); 
-//     void RenderAll(Shader &Shader);
-// private:    
-//     std::unordered_map<std::string, Model> models;
-// };
+#include <unordered_map>
+#include <memory>
+#include <string>
+
+class Model;
+
+class ModelManager
+{
+public:
+    static std::shared_ptr<Model> Load(const std::string &path); 
+    static std::shared_ptr<Model> Get(const std::string &path); 
+    static void Clear();
+private:    
+    static std::unordered_map<std::string, std::shared_ptr<Model>> s_models;
+};
+
+#endif
